4e06607a155e7207821927f9ce04ec0cfecb163f
Warren Clough <clough.warren@gmail.com>
2014-04-13 11:06:28 -0500

    Initial site configuration done

---
+++ source/site/configuration/site-header.php
@@ -0,0 +1,3 @@
+
+<script type="text/javascript" scr="<?php echo $_SERVER['SERVER_NAME'];?>/db-assignment2/source/site/javascript/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" scr="<?php echo $_SERVER['SERVER_NAME'];?>/db-assignment2/source/site/semantic.js"></script>
