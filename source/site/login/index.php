8db55099b3369bf6e4b0a81adb6b14a4dd027f09
Warren Clough <clough.warren@gmail.com>
2014-04-14 04:59:49 -0500

    Updated login script

--- source/site/login/index.php
+++ source/site/login/index.php
@@ -2,6 +2,7 @@
 <head>
 	<title>Home</title>
 	<?php 
+		session_start();
 		error_reporting(0);
 		define ('APP_ROOT',substr(dirname(__FILE__),0, strrpos(dirname(__FILE__),'/',-1)));
 		include APP_ROOT.'/configuration/site-header.php' 
@@ -12,7 +13,13 @@
 		<div class="container">
 			<div class="login-area">
 				<h1 claas="ui header">Login</h1>
-				<form class="ui form" method="POST" action="http://<?php echo $_SERVER['SERVER_NAME'];?>/db-assignment2/source/site/login/login.php">
+				<form  class="ui error form segment" method="POST" action="http://<?php echo $_SERVER['SERVER_NAME'];?>/db-assignment2/source/site/login/login.php">
+					<?php if ($_SESSION['error_msg'] != ''): ?>
+						<div class="ui error message">
+							<?php echo "Session data : ". $_SESSION['error_msg']; ?>
+							<?php $_SESSION['error_msg'] = null; ?>
+						</div>						
+					<?php endif;?>
 					<div>
 						<p>Have a account?Register <a href="http://<?php echo $_SERVER['SERVER_NAME'];?>/db-assignment2/source/site/register">Here</a></p>
 					</div>
--- source/site/login/login.php
+++ source/site/login/login.php
@@ -1,32 +1,38 @@
 <?php 
 session_start();
+define ('APP_ROOT',substr(dirname(__FILE__),0, strrpos(dirname(__FILE__),'/',-1)));
+include APP_ROOT.'/configuration/hash-key.php';
+include APP_ROOT.'/configuration/config.php';
 
 $user_email = $_POST['user_email'];
-$user_password = $_POST['password'];
+$user_password = crypt($_POST['user_password'],HASH_KEY);
+var_dump($user_password);
 
 //open a db connection
-$mysqli = new mysqli('localhost', 'root', '', 'db_assignment2');
+$mysqli = new mysqli(DBHOST, DBUSER, DBPASSWORD, DBNAME);
 if($mysqli->connect_errno > 0)
 	die('Unable to connect to the database ['.$mysqli->connect_error.']');
 
-$resultSet = $mysqli->query("CALL lookUpUser ('$user_email')");
-$results  = mysqli_fetch_array($resultSet);
-var_dump($results);
+$resultSet = $mysqli->query("SELECT * FROM user WHERE email = '$user_email'");
+$results = $resultSet->fetch_array();
 
 if(count($results)!=0)
 {
 	if($results['password']==$user_password)
 	{
 		$_SESSION['user'] = $results['user_id'];
+		header("Location: http://".$_SERVER['SERVER_NAME']."/db-assignment2/source/site/home");
 	}
 	else
 	{
 		$_SESSION['error_msg'] = "The email or password entered is incorrect";
+		header("Location: http://".$_SERVER['SERVER_NAME']."/db-assignment2/source/site/login");
 	}
 }
 else
 {
 	$_SESSION['error_msg'] = "The email or password entered is incorrect";
+	header("Location: http://".$_SERVER['SERVER_NAME']."/db-assignment2/source/site/login");
 }
 
 $mysqli->close();
