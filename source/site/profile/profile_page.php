59e1d4b87dbecedb5505b73e49b8d526505e3bc8
Shane Campbell <shanec132006@gmail.com>
2014-04-15 14:46:45 -0500

    Updated profile page to pull user posts

--- source/site/profile/profile_page.php
+++ source/site/profile/profile_page.php
@@ -3,8 +3,10 @@
 	require_once('../services/auth.php');
 	include '../header/header.php';
 	require_once('../services/global_queries.php');
+	 include '../post/post-functions.php';
 	
-?>
+?>	
+	
 
 		<div class = "content">
 
@@ -52,6 +54,41 @@
 				<div class = "eleven wide column">
 					<div class="ui segment">
 
+					<?php 
+							//get all the post for the current user
+							$user_id = $_SESSION['user'];
+							$user_posts = getAllPost($user_id);
+						?>
+						<?php	if(empty($user_posts)): ?>
+							<div style="margin-top: 20px;"> 
+								<p> You currently have no post. </p>
+							</div>
+						<?php endif;?>
+
+
+						<?php foreach($user_posts as $user_post): ?>
+							<div class="ui post">
+								<div class="post-header">
+									<img class="profile-pic" src="http://<?php echo SERVER;?>/db-assignment2/source/site/resources/images/profile_pics/default-user.png">
+									<h3>
+										<a href="http://<?php echo SERVER; ?>/db-assignment2/source/site/profile/profile_page.php"> 
+										<?php echo $user_post['fname']." ".$user_post['lname']; ?></a>
+									</h3>
+									<p>Created: <?php echo $user_post['date_created'];?></p>
+									<hr>
+								</div>
+								<?php if($user_post['image_path']!=""): ?>
+									<div class="post-image">
+										<img src="<?php echo $user_post['image_path']; ?>">
+									</div>
+								<?php endif; ?>
+								<div class="column  post-body">
+									<?php echo $user_post['text_body']; ?>
+								</div>
+								<div class="post-footer">
+								</div>
+							</div>
+						<?php  endforeach; ?>						
 					</div>
 				</div>
 
