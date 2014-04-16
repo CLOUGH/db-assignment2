0d07f61b5073212fc886f3972f7794783425a10a
Shane Campbell <shanec132006@gmail.com>
2014-04-16 14:20:12 -0500

    Db Update

--- source/database/db_assignment2_orginal.sql
+++ source/database/db_assignment2_orginal.sql
@@ -148,7 +148,97 @@ DELIMITER //
 	DELIMITER ;
 DELIMITER $$
 
+DELIMITER //
+	 CREATE PROCEDURE CreateUser(IN status varchar(255), password varchar(255), email varchar(255))
+	 BEGIN
+		INSERT INTO `user` ( `status`, `password`, `email`) VALUES (status, password, email);
+	 END //
+	DELIMITER ;
+DELIMITER $$
+
 /*
 ADMIN
 */
-INSERT INTO  user()
+INSERT INTO `add_to_group` (`user_id`, `group_id`, `date_added`) VALUES
+(3, 3, '2014-04-16'),
+(3, 4, '2014-04-16'),
+(3, 5, '2014-04-16'),
+(3, 6, '2014-04-16'),
+(3, 7, '2014-04-16'),
+(5, 6, '2014-04-16'),
+(6, 3, '2014-04-16');
+
+INSERT INTO `comment` (`comment_id`, `content`) VALUES
+(1, 0x54636f6d65),
+(2, 0x4920636f6d6d656e746564206f6e207468697320706f7374),
+(3, 0x736166),
+(4, 0x796f77);
+
+INSERT INTO `comments_on` (`post_id`, `user_id`, `comment_id`, `date_commented`) VALUES
+(1, 3, 1, '2014-04-15'),
+(1, 3, 2, '2014-04-15'),
+(1, 3, 3, '2014-04-15'),
+(1, 3, 4, '2014-04-15');
+
+INSERT INTO `creates` (`post_id`, `user_id`, `date_created`) VALUES
+(1, 3, '2014-04-15'),
+(2, 3, '2014-04-15'),
+(3, 3, '2014-04-15'),
+(4, 3, '2014-04-15');
+
+INSERT INTO `create_content` (`user_id`, `group_id`, `gpost_id`, `date_created`) VALUES
+(3, 3, 4, '2014-04-16'),
+(3, 3, 5, '2014-04-16'),
+(3, 6, 6, '2014-04-16'),
+(5, 3, 3, '2014-04-16');
+
+INSERT INTO `create_group` (`group_id`, `user_id`, `date_created`) VALUES
+(0, 3, '2014-04-16'),
+(3, 3, '2014-04-16'),
+(4, 3, '2014-04-16'),
+(5, 3, '2014-04-16'),
+(6, 3, '2014-04-16'),
+(7, 3, '2014-04-16'),
+(8, 5, '2014-04-16');
+
+
+INSERT INTO `friend_of` (`friend_owner`, `friend`, `type`) VALUES
+(3, 5, 'friend'),
+(5, 3, 'friend'),
+(5, 6, 'friend'),
+(6, 3, 'friend'),
+(6, 5, 'friend');
+
+INSERT INTO `group_post` (`gpost_id`, `title`, `g_post_type`, `g_image_path`, `text_body`) VALUES
+(1, 'This a Group Post', 'Group Post', 'tesgt', 0x6564676b6d736764707367),
+(3, 'Testing the assiciation rela', 'sdg', 'hf', 0x66646873),
+(4, 'The New Group Post', 'sample', '', 0x546869732067726f757020697320676f696e6720746f206265207468652062657374207468696e672073696e636520736c696365206272656164),
+(5, 'Final Post', 'info', '', 0x596f207520677579732073617720746865206e6577206e617275746f206d616e6761),
+(6, 'This is a Group Post ', '', '', 0x54686973206973207468652073616d652070706c65617365);
+
+INSERT INTO `post` (`post_id`, `post_type`, `image_path`, `text_body`) VALUES
+(1, 'test', NULL, NULL),
+(2, 'user_post', '', 0x5465657374),
+(3, 'user_post', '', 0x726f6e696e73666c6b6e6470666f6e6d7064666d6e645b66),
+(4, 'user_post', '', 0x78766e622c6d2066766b2f6c0d0a);
+
+INSERT INTO `profile` (`user_id`, `fname`, `lname`, `dob`, `profile_pic`) VALUES
+(3, 'Shane', 'Campbell', '1970-01-01', '../images/profile_pics/SL381711_crop.jpg'),
+(5, 'Warren', 'Clough', '1970-01-01', '../images/profile_pics/IMG_20140307_091259520.jpg'),
+(6, 'Kenrick', 'Beckett', '1991-06-10', NULL);
+
+INSERT INTO `user` (`user_id`, `status`, `password`, `email`) VALUES
+(3, 'active', 'a4TxbGfBrxO3Y', 'shanec132006@hotmail.com'),
+(5, 'active', 'a4TxbGfBrxO3Y', 'clough.warren@gmail.com'),
+(6, 'active', 'a4TxbGfBrxO3Y', 'kenrick1991@gmail.com');
+
+
+INSERT INTO `user_group` (`group_id`, `group_name`) VALUES
+(1, 'test'),
+(2, 'This is the new one'),
+(3, 'Another Group'),
+(4, ''),
+(5, ''),
+(6, 'Final Group'),
+(7, 'Second Group'),
+(8, '');
